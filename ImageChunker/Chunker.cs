c5e98085f29cef674b67f5d7baaab5fc8f10c89f
Andreas Grech <agnt666@gmail.com>
2011-03-05 12:19:27 +0100

    some refactoring (introducing the Chunker class) and validation

---
+++ ImageChunker/Chunker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace ImageChunker
+{
+    class Chunker
+    {
+        public int TotalImages { get; set; }
+        public int CropWidth { get; set; }
+        public int CropHeight { get; set; }
+
+        private readonly Image image;
+
+        public Chunker(Image image, int cropWidth, int cropHeight)
+        {
+            this.image = image;
+            CropWidth = cropWidth;
+            CropHeight = cropHeight;
+
+            TotalImages = (int)Math.Ceiling((double)image.Width / CropWidth);
+        }
+
+        public void SplitAndSave(string imageLocation)
+        {
+            var bmp = new Bitmap(CropWidth, image.Height);
+            Graphics g = GetGraphicsFromImage(bmp);
+
+            var baseName = Path.GetFileNameWithoutExtension(imageLocation);
+            for (int i = 0; i < TotalImages; i++)
+            {
+                var remainingWidth = CropWidth;
+                if (i == TotalImages - 1) // last image
+                {
+                    remainingWidth = image.Width - (CropWidth * (i));
+                    g = GetGraphicsFromImage(new Bitmap(remainingWidth, image.Height));
+                }
+                g.Clear(Color.Transparent);
+                g.DrawImage(image, new Rectangle(0, 0, remainingWidth, image.Height), new Rectangle(i * CropWidth, 0, remainingWidth, image.Height), GraphicsUnit.Pixel);
+                bmp.Save(String.Format("{0}{1}{2}", baseName, i, Path.GetExtension(imageLocation)));
+            }
+        }
+
+        private static Graphics GetGraphicsFromImage(Bitmap bmp)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            return g;
+        }
+    }
+}
--- ImageChunker/Program.cs
+++ ImageChunker/Program.cs
@@ -14,30 +14,11 @@ namespace ImageChunker
             Image image = Image.FromFile(imageLocation,true);
             if (width >= image.Width)
             {
-                return;
+                throw new Exception("Your specified width is >= than the total width of your image! Wtf?");
             }
 
-            var totalImages = (int)Math.Ceiling((double)image.Width/width);
-
-            var bmp = new Bitmap(width, image.Height);
-            Graphics g = Graphics.FromImage(bmp);
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-            var baseName = Path.GetFileNameWithoutExtension(imageLocation);
-            for (int i = 0; i < totalImages; i++)
-            {
-                var remainingWidth = width;
-                if (i == totalImages - 1) // last image
-                {
-                    remainingWidth = image.Width - (width*(i));
-                    g = Graphics.FromImage(bmp = new Bitmap(remainingWidth, image.Height));
-                }
-                g.Clear(Color.Transparent);
-                g.DrawImage(image, new Rectangle(0, 0, remainingWidth, image.Height), new Rectangle(i * width, 0, remainingWidth, image.Height), GraphicsUnit.Pixel);
-                bmp.Save(String.Format("{0}{1}{2}",baseName, i, Path.GetExtension(imageLocation)));
-            }
+            var chunker = new Chunker(image, width, 100);
+            chunker.SplitAndSave(imageLocation);
         }
     }
 }
