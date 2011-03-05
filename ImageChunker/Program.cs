abda9d0e083fc7bb4b81b02bdac28394e3510e6e
Andreas Grech <agnt666@gmail.com>
2011-03-05 13:34:26 +0100

    added command line options.  height still isnt functional

--- ImageChunker/Program.cs
+++ ImageChunker/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
+using NDesk.Options;
 
 namespace ImageChunker
 {
     class Program
     {
-        //TODO: Command Line Args
-
         static void Main(string[] args)
         {
-            string imageLocation = args[0];
-            int width = Convert.ToInt32(args[1]);
+            var settings = HandleArgs(args);
+            string imageLocation = settings.Image;
 
-            var chunker = new Chunker(imageLocation, width, 100);
+            var chunker = new Chunker(imageLocation, settings.Width, settings.Height);
             chunker.SplitAndSave();
+        }
+
+        static Settings HandleArgs(IEnumerable<string> args)
+        {
+            int width = 0, height = 0;
+            string image = "", output = "";
+            var options = new OptionSet
+                              {
+                                  {"w|width=", "The full width of the chunks", (int w) => { width = w; }},
+                                  {"h|height=", "The full height of the chunks", (int h) => { height = h; }},
+                                  {"o|output=", "Where do you want the chunks to be saved?  By default, they will be saved here.", (string o) => { output = o; }},
+                                  {"<>","The image path",(string i) => { image = i;}}
+                              };
+            options.Parse(args);
+            return new Settings {Image = image, Height = height, Width = width, OutputPath = output};
         }
     }
 }
---
+++ ImageChunker/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageChunker
+{
+    class Settings
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string OutputPath { get; set; }
+        public string Image { get; set; }
+    }
+}
